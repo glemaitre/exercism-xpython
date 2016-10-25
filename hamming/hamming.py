8e3aeaec1ae2ac284c15abf51f0fc674024ae24a
Joan Massich <sik@visor.udg.edu>
2016-10-25 13:12:06 +0200

    [hamming] pythonic flag generation

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,8 +1,3 @@
 def distance(s1, s2):
-    pairs = zip(s1, s2)
-    equal = [p[0]==p[1] for p in pairs]
-    _d = 0
-    for e in equal:
-        if not e:
-            _d += 1
-    return _d
+    diff = [1 for (a,b) in zip(s1, s2) if a!=b]
+    return sum(diff)
